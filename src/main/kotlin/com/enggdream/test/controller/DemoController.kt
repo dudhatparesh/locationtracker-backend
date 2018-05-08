8e3a6cc77040dd60f06dcc553c99257498776bf0
Paresh Dudhat <dudhatparesh@gmail.com>
2018-05-08 10:22:53 +0530

    added dummy location data

--- src/main/kotlin/com/enggdream/test/controller/DemoController.kt
+++ src/main/kotlin/com/enggdream/test/controller/DemoController.kt
@@ -2,11 +2,18 @@ package com.enggdream.test.controller
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 class DemoController {
     @GetMapping("/")
     fun indexPage(): String {
         return "index"
+    }
+    @GetMapping("/track")
+    fun track(): String{
+        return "track"
     }
 }
